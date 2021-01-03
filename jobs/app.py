a3c0bb5afadc2fca181dbebb22a978a6987df843
eloisaperea <75701585+eloisaperea@users.noreply.github.com>
2021-01-02 20:15:22 -0800

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, g
+
+PATH = db/jobs.sqlite
 
 app = Flask(__name__)
+def open_connection():
+    getattr(flask.g, '_connection', None)
+    return getattr(connection)
 
 @app.route('/')
 @app.route('/jobs')
