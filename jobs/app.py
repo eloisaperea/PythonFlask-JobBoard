c4909410dd8b343c5b3090de943b79ebf6287ddc
eloisaperea <75701585+eloisaperea@users.noreply.github.com>
2021-01-02 20:41:48 -0800

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -7,7 +7,18 @@ app = Flask(__name__)
 def open_connection():
     getattr(g, '_connection', None)
     if '_connection' == None:
-    return getattr(connection)
+        sqlite3.connect(PATH) = "connection"
+        sqlite3.connect(PATH) = 'g._connection'
+    'connection'.row_factory = 'sqlite3.Row'
+    return 'connection'
+    return getattr('connection')
+
+def execute_sql(sql, values, commit, single):
+    'connection' = 'open_connection'
+    'values' = ()
+    'commit' = False
+    'single' = False
+
 
 @app.route('/')
 @app.route('/jobs')
