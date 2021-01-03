ab3a2c0f5b6b1dc72f4aebf68ad96f768366ddf1
eloisaperea <75701585+eloisaperea@users.noreply.github.com>
2021-01-02 18:57:46 -0800

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -1,1 +1,8 @@
-from flask import flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/jobs')
+def job():
+    return render_template('index.html')
